54492f3dae762d56575ef0b8fb8e08d768852d14
Aljay <ayago@twitter.com>
2021-09-19 19:01:22 +0800

    fix output

--- yaml.py
+++ yaml.py
@@ -4,10 +4,10 @@ import yaml
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-n','--name',type=str,dest='name',action='store',help="Hostname",required=True)
-parser.add_argument('-s','--size',choices=['small','medium','large','xlarge'],action='store', dest='size',required=True)
-parser.add_argument('-d','--dc',choices=['smfc', 'atlc'], action='store', dest='dc', required=True,help='Datacenter')
-parser.add_argument('-os',choices=['centos7','debian10','win16'],action='store',dest='os',required=True,help="OS")
-parser.add_argument('-ip',action='store',required=True,dest='ip',help="IP")
+parser.add_argument('-s','--size',choices=['small','medium','large','xlarge'],action='store', dest='size',default='small')
+parser.add_argument('-d','--dc',choices=['smfc', 'atlc'], action='store', dest='dc', help='Datacenter',default='smfc')
+parser.add_argument('-os',choices=['centos7','debian10','win16'],action='store',dest='os',help="OS",default='centos7')
+parser.add_argument('-ip',action='store',dest='ip',help="IP",default='None')
 parser.add_argument('-dev',default='dev')
 parser.add_argument('-u', action='store', dest='username', required=True,help='VSphere Username')
 parser.add_argument('-f', action='store', dest='vm_file',help="input file containing")
@@ -15,8 +15,8 @@ parser.add_argument('-f', action='store', dest='vm_file',help="input file contai
 parsed = parser.parse_args()
 
 if parsed.vm_file == None:
-    print(parsed.name,parsed.size,parsed.dc,parsed.os,parsed.ip,parsed.dev)
+    vm_list=(parsed.name,parsed.size,parsed.dc,parsed.os,parsed.ip,parsed.dev)
 else:
     with open(parsed.vm_file,'r') as file:
         vm_list = [line.replace(" ",",").strip() for line in file.readlines()]
-    print(vm_list)
+print(vm_list)
